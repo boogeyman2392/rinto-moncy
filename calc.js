177364ec6bb74320982038c00cd0947943b703f6
Rinto Moncy <110585429+boogeyman2392@users.noreply.github.com>
2023-12-06 12:40:55 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,6 @@
+function add(){
+    let a=parseInt(document.getElementById("n1").value);
+    let b=parseInt(document.getElementById("n2").value);
+    let c=a+b;
+    document.getElementById("button1").innerHTML="Addition of given number"+c;
+}
